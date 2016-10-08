add419cf91bf0c73b58744e60a39397de634f984
faizmaricar <maricar.faiz@gmail.com>
2016-10-09 04:33:41 +0800

    added filtering publisher[A

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
 $(document).ready(
     function(){
-        var owl = $("#owl-example")
-        owl.owlCarousel({
-            slideSpeed : 500,
-            paginationSpeed : 400,
-            singleItem:true,
-            pagination: false,
-            jsonPath: "script/items.json",
-            jsonSuccess: displayItems
-        });
+        var next = $(".next");
+        var prev = $(".prev");
+        var filter = $("#filter");
+        var filtered = false;
+        var ajaxOptions = {
+            url: "script/items.json",
+            dataType: "json",
+            type: "get",
+            cache: false,
+            success: displayItems
+        };
+        $.ajax(ajaxOptions);
+        
         function displayItems(items){
             loadForm(items);
-            var $carousel = $("#owl-example");
-            
+            var $carousel = $(".carousel");
             items.forEach(
                 function(item, index){
                     var $row = $("<div></div>");
-                    $row.addClass(item.Title.replace(/\s+/g, ''))
-                    $row.addClass(item.Publisher.replace(/\s+/g, ''))
-                    $row.addClass(item.ItemCode)
-                    $row.addClass(item.ReleaseDate.slice(5).replace(/\s+/g, ''))
-                    
+                    $row.addClass("item");
+                    $row.addClass(item.Publisher.trim().split(" ").join(""));
                     var $hgroup = $("<hgroup></hgroup>").attr("class", "col-sm-12");
                     var $article = $("<article></article>").attr("class", "col-sm-8")
                     var $section = $("<section></section>") .attr("class", "col-sm-4");
@@ -28,7 +28,7 @@ $(document).ready(
                     var $title = $("<h1></h1>").html("<strong>" + item.Title + "</strong>")
                     var $publisher = $("<h2></h2>").html("<strong>" + item.Publisher + "</strong>")
                     
-                    var $img = $("<img>").attr({src: item.mainContentImage, class: "img-responsive"});
+                    var $img = $("<img>").attr({src: item.mainContentImage, alt: "Item Image", class: "img-responsive"});
                     var $itemcode = $("<h3></h3>").html("<strong>" + item.ItemCode + "</strong>").attr("class", "col-sm-12");
                     
                     var $creators = $("<p></p>").html("<strong>" + item.Creators + "</strong>").attr("class", "col-sm-12");
@@ -54,48 +54,50 @@ $(document).ready(
                     $carousel.append($row);
                     $("#totalItems").text(items.length);
                 });
+            $(".carousel").slick({arrows: false});
         }
         
         function loadForm(items){
             var publishers = [""];
-            var srp = [""];
-            
             var $publishers = $("#publisher");
-            var $srp = $("#srp");
             
             items.forEach(
                 function(item, index){
                     if(!publishers.includes(item.Publisher.trim())){
                         publishers.push(item.Publisher.trim());   
                     }
-                    if(!srp.includes(parseFloat(item.SRP.trim().slice(6)))){
-                        srp.push(parseFloat(item.SRP.trim().slice(6)));   
-                    }
                 }
             );
             publishers = publishers.sort();
-            srp = srp.sort(function(a,b){return a-b});
             publishers.forEach(
                 function(publisher, index){
                     var $publisher = $("<option></option>").html(publisher).attr("value", publisher.split(" ").join(""));
                     $publishers.append($publisher);
                 }
             );
-            srp.forEach(
-                function(price, index){
-                    if(price !== ""){
-                        price = "$" + price
-                    }
-                    var $price = $("<option></option>").html(price).attr("value", price);
-                    $srp.append($price);
-                }
-            );
         }
-        $(".next").click(function(){
-            owl.trigger('owl.next');
-        })
-        $(".prev").click(function(){
-            owl.trigger('owl.prev');
-        })
+        next.click(
+            function(){
+                $(".carousel").slick("slickNext");
+            }
+        );
+        prev.click(
+            function(){
+                $(".carousel").slick("slickPrev");
+                
+            }
+        );
+        filter.click(
+            function(){
+                if($("#publisher").val() === ""){
+                    $(".carousel").slick('slickUnfilter');
+                }
+                else{
+                    $(".carousel").slick('slickUnfilter');
+                    $(".carousel").slick('slickFilter', '.' + $("#publisher").val());
+                }
+                
+            }
+        );
     }
 );
